04c98793aeaad79efcc77b563139519d1eb3974d
Juanmeta <juanmabarrio@gmail.com>
2019-11-10 15:11:55 +0100

    Refactorizo Game de nuevo. Primero simplifico move, creando un metodo local canMove() para la validacion del movimiento

--- damas/draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Game.java
+++ damas/draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Game.java
@@ -36,35 +36,47 @@ public class Game {
 		return null;
 	}
 
-	public Error move(Coordinate origin, Coordinate target) {
+	public Error canMove(Coordinate origin, Coordinate target){
+        if (board.isEmpty(origin)) {
+            return Error.EMPTY_ORIGIN;
+        }
+        Color color = this.board.getColor(origin);
+        if (this.turn.getColor() != color) {
+            return Error.OPPOSITE_PIECE;
+        }
+        if (!origin.isDiagonal(target)) {
+            return Error.NOT_DIAGONAL;
+        }
+        Piece piece = this.board.getPiece(origin);
+//       Error error = piece.canMove(target);
+        if (!piece.isAdvanced(origin, target)) {
+            return Error.NOT_ADVANCED;
+        }
+        if (origin.diagonalDistance(target) >= 3) {
+            return Error.BAD_DISTANCE;
+        }
+        if (!this.board.isEmpty(target)) {
+            return Error.NOT_EMPTY_TARGET;
+        }
+        if (origin.diagonalDistance(target) == 2) {
+            Coordinate between = origin.betweenDiagonal(target);
+            if (this.board.getPiece(between) == null) {
+                return Error.EATING_EMPTY;
+            }
 
-		if (board.isEmpty(origin)) {
-			return Error.EMPTY_ORIGIN;
-		}
-		Color color = this.board.getColor(origin);
-		if (this.turn.getColor() != color) {
-			return Error.OPPOSITE_PIECE;
-		}
-		if (!origin.isDiagonal(target)) {
-			return Error.NOT_DIAGONAL;
-		}
-		Piece piece = this.board.getPiece(origin);
-		if (!piece.isAdvanced(origin, target)) {
-			return Error.NOT_ADVANCED;
-		}
-		if (origin.diagonalDistance(target) >= 3) {
-			return Error.BAD_DISTANCE;
-		}
-		if (!this.board.isEmpty(target)) {
-			return Error.NOT_EMPTY_TARGET;
-		}
-		if (origin.diagonalDistance(target) == 2) {
-			Coordinate between = origin.betweenDiagonal(target);
-			if (this.board.getPiece(between) == null) {
-				return Error.EATING_EMPTY;
-			}
-			this.board.remove(between);
-		}
+        }
+        return null;
+    }
+
+	public Error move(Coordinate origin, Coordinate target) {
+       Error error = canMove(origin,target);
+       if (error != null){
+           return error;
+       }
+       if (origin.diagonalDistance(target) == 2){
+           Coordinate between = origin.betweenDiagonal(target);
+           this.board.remove(between);
+       }
 		this.board.move(origin, target);
 		this.turn.change();
 		return null;
