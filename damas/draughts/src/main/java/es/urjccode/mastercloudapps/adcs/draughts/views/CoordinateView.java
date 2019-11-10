6d2debe3315c70d7d1647591ccba35c79ff59d7b
Juanmeta <juanmabarrio@gmail.com>
2019-11-10 14:10:35 +0100

    Empezamos a limpiar el metodo Game.move(). Primero movemos la validacion de origin y target hacia CoordinateView vista, y aniadiamos un bucle hasta que el usuario introduzca un valor dentro de los margenes del board

--- damas/draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/CoordinateView.java
+++ damas/draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/CoordinateView.java
@@ -1,16 +1,25 @@
 package es.urjccode.mastercloudapps.adcs.draughts.views;
 
 import es.urjccode.mastercloudapps.adcs.draughts.models.Coordinate;
+import es.urjccode.mastercloudapps.adcs.draughts.models.Error;
 
 public class CoordinateView extends SubView {
 
     public Coordinate[]  readOriginAndTarget(String message) {
-        String command = this.console.readString(message);
-        Coordinate origin = new Coordinate().fromString(command.substring(0, 2));
-        Coordinate target = new Coordinate().fromString(command.substring(3, 5));
-        Coordinate[] coordinates = {origin,target};
-        return coordinates;
+        String command;
+        Error error=null;
+        Coordinate origin,target;
+        do {
+            command = this.console.readString(message);
+            origin = new Coordinate().fromString(command.substring(0, 2));
+            target = new Coordinate().fromString(command.substring(3, 5));
+            if (!origin.isValid() || !target.isValid()) {
+                error = Error.OUT_COORDINATE;
+                this.console.writeln(error.name());
 
+            }
+        }while(null!=error);
+        return new Coordinate[]{origin,target};
     }
 
 }
