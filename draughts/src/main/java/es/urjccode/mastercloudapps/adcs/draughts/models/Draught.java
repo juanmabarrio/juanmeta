cb2ea15ddb8c947dcf082f4fbd34174abc04dba3
Juanmeta <juanmabarrio@gmail.com>
2019-11-17 21:47:21 +0100

    Pienso en un caso significativo para probar la dama. Se me ocurre uno, actualizo DraughtsTest solo con este test. El resto venian copiados de PawnTest

--- draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Draught.java
+++ draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Draught.java
@@ -1,5 +1,8 @@
 package es.urjccode.mastercloudapps.adcs.draughts.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class Draught extends Piece {
 
     Draught(Color color) {
@@ -7,22 +10,56 @@ class Draught extends Piece {
     }
 
 
-
     Error isCorrectMoveForMyTypeOfPiece(Coordinate origin, Coordinate target, PieceProvider pieceProvider) {
 
-      int distance = origin.diagonalDistance(target);
-      if (distance > 2 && pieceProvider.twoPiecesTogetherInBetween(origin,target)){
-          return Error.TOO_MANY_PIECES_IN_BETWEEN;
-      }
-      if (pieceProvider.isThereAPieceOfThisColorInBetween(this.getColor(),origin,target)){
-          return Error.CANT_EAT_YOUR_PIECES;
-      }
+        int distance = origin.diagonalDistance(target);
+        if (distance > 2 && pieceProvider.twoPiecesTogetherInBetween(origin, target)) {
+            return Error.TOO_MANY_PIECES_IN_BETWEEN;
+        }
+        if (pieceProvider.isThereAPieceOfThisColorInBetween(this.getColor(), origin, target)) {
+            return Error.CANT_EAT_YOUR_PIECES;
+        }
 
-      return null;
+        return null;
     }
 
     @Override
     boolean isBlocked(PieceProvider board, Coordinate coordinate) {
+        int row = coordinate.getRow();
+        int column = coordinate.getColumn();
+        int rowShift;
+        Piece piece;
+        rowShift = (this.getColor() == Color.BLACK) ? +1 : -1;
+        Set<Integer> columnShifts = new HashSet<>();
+        columnShifts.add(-1);
+        columnShifts.add(1);
+        if (column == 0)
+            columnShifts.remove(-1);
+        if (column == board.getDimension() - 1)
+            columnShifts.remove(1);
+        //first level search.
+        for (int columnShift : columnShifts) {
+            piece = board.getPiece(new Coordinate(row + rowShift, column + columnShift));
+            if (piece == null) {
+                return false;
+            }
+            if (piece.getColor() == this.getColor()) {
+                return true;
+            }
+        }
+        //second level search
+        if (column == 1)
+            columnShifts.remove(-1);
+        if (column == board.getDimension() - 2)
+            columnShifts.remove(1);
+        for (int columnShift : columnShifts) {
+            piece = board.getPiece(new Coordinate(row + rowShift + rowShift, column + columnShift + columnShift));
+            if (piece == null) {
+                return false;
+            } else return true;
+        }
         return false;
     }
 }
+
+
--- draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/DraughtTest.java
+++ draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/DraughtTest.java
@@ -7,75 +7,19 @@ import static org.junit.Assert.assertTrue;
 public class DraughtTest {
 
     @Test
-    public void testIsWhiteDraughtBlockedWhenOnFirstColumnAndTwoEnemiesBlocking() {
+    public void testIsWhiteDraughtNotBlockedWhenOnSecondRowAndSpaceBehind() {
         Game game = new GameBuilder()
             .row("        ")
-            .row("    b   ")
             .row("        ")
             .row("        ")
+            .row("n   n    ")
+            .row(" n n    ")
+            .row("  B     ")
             .row("        ")
-            .row("  n     ")
-            .row(" n      ")
-            .row("B       ")
+            .row("        ")
             .turn(new Turn())
             .build();
         Coordinate coordinate = new Coordinate(7, 0);
         assertTrue(game.getPiece(coordinate).isBlocked(game.getBoard(), coordinate));
     }
-
-    @Test
-    public void testIsBlackDraughtBlockedWhenOnFirstColumnAndTwoEnemiesBlocking() {
-        Turn blackTurn = new Turn();
-        blackTurn.change();
-        Game game = new GameBuilder()
-            .row("N       ")
-            .row(" b      ")
-            .row("  b     ")
-            .row("        ")
-            .row("        ")
-            .row("        ")
-            .row("        ")
-            .row("        ")
-            .turn(blackTurn)
-            .build();
-        Coordinate coordinate = new Coordinate(0, 0);
-        assertTrue(game.getPiece(coordinate).isBlocked(game.getBoard(), coordinate));
-    }
-
-    @Test
-    public void testIsWhiteDraughtBlockedWhenOnFirstColumnAndAnotherWhiteBlocking() {
-        Game game = new GameBuilder()
-            .row("        ")
-            .row("    b   ")
-            .row("        ")
-            .row("        ")
-            .row("        ")
-            .row("        ")
-            .row(" b      ")
-            .row("B       ")
-            .turn(new Turn())
-            .build();
-        Coordinate coordinate = new Coordinate(7, 0);
-        assertTrue(game.getPiece(coordinate).isBlocked(game.getBoard(), coordinate));
-    }
-
-    @Test
-    public void testIsBlackDraughtBlockedWhenOnFirstColumnAndAnotherBlackBlocking() {
-        Turn blackTurn = new Turn();
-        blackTurn.change();
-        Game game = new GameBuilder()
-            .row("N       ")
-            .row(" n      ")
-            .row("        ")
-            .row("        ")
-            .row("        ")
-            .row("        ")
-            .row("        ")
-            .row("        ")
-            .turn(blackTurn)
-            .build();
-        Coordinate coordinate = new Coordinate(0, 0);
-        assertTrue(game.getPiece(coordinate).isBlocked(game.getBoard(), coordinate));
-    }
-
 }
