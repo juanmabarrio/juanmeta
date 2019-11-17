1ce4681bdbc0a5dfe669f8ccd4fb1a81ed0f3131
Juanmeta <juanmabarrio@gmail.com>
2019-11-17 20:59:10 +0100

    Aniado tests para testear el block de piece cuando piece esta en la ultima columna. Los tests fallan

--- draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/PawnTest.java
+++ draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/PawnTest.java
@@ -80,5 +80,79 @@ public class PawnTest {
     }
 
 
+    @Test
+    public void testIsWhitePawnBlockedWhenOnLastColumnAndTwoEnemiesBlocking(){
+        Game game = new GameBuilder()
+            .row("        ")
+            .row("        ")
+            .row("        ")
+            .row("        ")
+            .row("        ")
+            .row("     n  ")
+            .row("      n ")
+            .row("       b")
+            .turn(new Turn())
+            .build();
+        Coordinate coordinate = new Coordinate(7,7);
+        assertTrue(game.getPiece(coordinate).isBlocked(game.getBoard(), coordinate));
+    }
+
+    @Test
+    public void testIsBlackPawnBlockedWhenOnLastColumnAndTwoEnemiesBlocking(){
+        Turn blackTurn = new Turn();
+        blackTurn.change();
+        Game game = new GameBuilder()
+            .row("       n")
+            .row("      b ")
+            .row("     b  ")
+            .row("        ")
+            .row("        ")
+            .row("        ")
+            .row("        ")
+            .row("        ")
+            .turn(blackTurn)
+            .build();
+        Coordinate coordinate = new Coordinate(0,7);
+        assertTrue(game.getPiece(coordinate).isBlocked(game.getBoard(), coordinate));
+    }
+
+    @Test
+    public void testIsWhitePawnBlockedWhenOnLastColumnAndAnotherWhiteBlocking(){
+        Game game = new GameBuilder()
+            .row("        ")
+            .row("    b   ")
+            .row("        ")
+            .row("        ")
+            .row("        ")
+            .row("        ")
+            .row("      b ")
+            .row("       b")
+            .turn(new Turn())
+            .build();
+        Coordinate coordinate = new Coordinate(7,7);
+        assertTrue(game.getPiece(coordinate).isBlocked(game.getBoard(), coordinate));
+    }
+
+    @Test
+    public void testIsBlackPawnBlockedWhenOnLastColumnAndAnotherWhiteBlocking(){
+        Turn blackTurn = new Turn();
+        blackTurn.change();
+        Game game = new GameBuilder()
+            .row("       n")
+            .row("      n ")
+            .row("        ")
+            .row("        ")
+            .row("        ")
+            .row("        ")
+            .row("        ")
+            .row("        ")
+            .turn(blackTurn)
+            .build();
+        Coordinate coordinate = new Coordinate(0,7);
+        assertTrue(game.getPiece(coordinate).isBlocked(game.getBoard(), coordinate));
+    }
+
+
+
 
 }
